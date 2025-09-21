bd5a8acbdb70b5ee8224baeae449ddcb2a699e5b
atrepalin <atrepalin19@gmail.com>
2025-09-21 14:53:31 +0300

    Rewrite symbol to return void pointer

--- labs/load.c
+++ labs/load.c
@@ -9,18 +9,18 @@ int main()
     int (*sum)(int, int), (*sub)(int, int), (*mul)(int, int), (*div)(int, int);
     void (*foo_main)(void), (*bar_main)(void);
 
-    foo_main = (void (*)(void))symbol("main", foo);
-    bar_main = (void (*)(void))symbol("main", bar);
+    foo_main = symbol("main", foo);
+    bar_main = symbol("main", bar);
 
     foo_main();
     bar_main();
 
     if(fork() == 0)
     {
-        sum = (int (*)(int, int))symbol("sum", foo);
-        sub = (int (*)(int, int))symbol("sub", foo);
-        mul = (int (*)(int, int))symbol("mul", bar);
-        div = (int (*)(int, int))symbol("div", bar);
+        sum = symbol("sum", foo);
+        sub = symbol("sub", foo);
+        mul = symbol("mul", bar);
+        div = symbol("div", bar);
 
         printf("%d + %d = %d\n", 4, 5, sum(4, 5));
         printf("%d - %d = %d\n", 4, 5, sub(4, 5));
--- user/user.h
+++ user/user.h
@@ -35,7 +35,7 @@ int log_time(void);
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, size_t offset);
 int munmap(void *addr, size_t length);
 int load(const char*);
-uint64 symbol(const char*, int);
+void* symbol(const char*, int);
 
 // ulib.c
 int stat(const char*, struct stat*);
